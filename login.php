5b7ee2cf27cdd91ee09ccc0f19f5ac2329ea4e4e
EC2 Default User <ec2-user@ip-172-31-18-31.us-west-2.compute.internal>
2014-10-04 06:49:44 +0000

    messing around with sqlite3

--- login.php
+++ login.php
@@ -1,37 +1,64 @@
 <?php
 
 
-  $user= $_POST['user']
+  $user= $_POST['user'];
     $_POST['password'];
 
-    $user = 'paul';
+    $user = 'PAUL';
+   echo $user;
+   $db = new SQLite3('outdare.db');
  
-    $db = new SQLite3->('outdare.db');
- 
-    $(!$db){
+  if(!$db){
 
 	echo $db->lastErrorMsg();
     }
     else{
 
 	echo "OPENED DATABASE BRUH!";
-    }
- //  $queries = "SELECT user, email, phonenumber WHERE username=:user";
+   }
+  //$queries = 'SELECT username, email, phonenumber FROM users WHERE username = :user;';
+
+  $queries = 'SELECT * FROM users';
+
+
+	$statement = $db->query($queries);
+//    $statement->bindValue(':user',$user);
+
+//	$result = $statement->execute();
+
+	while($row = $results->fetchArray()){
+
+		var_dump($row);
+	}
 
 
 
-  //  $statement = $db->prepare($queries);
- //   $statement->bindValue(':user', $user,SQLITE3_TEXT);
-    
-
-   // $result = $statement->execute();
 
 
-    //echo $result;
+//	echo $result;
+
+
+
+	//$row;
+
+//	while($row = $statement->fetchArray()){
+
+//	var_dump($row);
+//	}
+
+ //   $statement = $db->prepare($queries);
+/*	echo $statement;   
+ $statement->bindValue(':user', $user,SQLITE3_TEXT);
+ echo $statement;  
+
+    $result = $statement->execute();
+
+
+   echo $result;
 
    // var_dump($result->fetchArray());
 
-
+*/
     
 
 
