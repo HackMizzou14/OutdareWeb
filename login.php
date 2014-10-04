570fadce48bab70ecc225681633cae2ac8cb47c0
EC2 Default User <ec2-user@ip-172-31-18-31.us-west-2.compute.internal>
2014-10-04 06:05:51 +0000

    trying to get sqllite to work

--- login.php
+++ login.php
@@ -1,23 +1,33 @@
 <?php
 
+
   $user= $_POST['user']
     $_POST['password'];
 
     $user = 'paul';
+ 
+    $db = new SQLite3->('outdare.db');
+ 
+    $(!$db){
+
+	echo $db->lastErrorMsg();
+    }
+    else{
+
+	echo "OPENED DATABASE BRUH!";
+    }
+ //  $queries = "SELECT user, email, phonenumber WHERE username=:user";
+
+
+
+  //  $statement = $db->prepare($queries);
+ //   $statement->bindValue(':user', $user,SQLITE3_TEXT);
     
 
-    $db = new SQLite3('outdare.db');
-    $queries = "SELECT user, email, phonenumber WHERE username=:user";
-  //  $db->exec($queries);
+   // $result = $statement->execute();
 
 
-    $statement = $db->prepare($queries);
-    $statement->bindValue(':user', $user,SQLITE3_TEXT);
-    
-
-    $result = $statement->execute();
-
-    echo $result;
+    //echo $result;
 
    // var_dump($result->fetchArray());
 
