2be46dbf4cc1eeaaa8a754b254ddcca81d8a54c8
Sammons <bsammnz@gmail.com>
2014-10-04 17:14:05 -0500

    testing submission

--- OutDare/endpoints/submissions.js
+++ OutDare/endpoints/submissions.js
@@ -3,14 +3,19 @@
 module.exports.url = '/submissions/create';
 module.exports.verb = 'post';
 module.exports.handler = function (req, res) {
-    console.log(req.body);
-    var submission = new database.submission({
-        user_id : req.body.user_id,
-        dare_id : req.body.dare_id,
-        image   : req.body.image
-    });
-    submission.save(function (e) {
-        if (e) throw ("failed to create new submission", e);
-        res.end();
-    });
+    var body = '';
+    req.body.on('data', function (chunk) {
+        body += chunk;
+        console.log('data');
+    })
+    res.end('test');
+    //var submission = new database.submission({
+    //    user_id : req.body.user_id,
+    //    dare_id : req.body.dare_id,
+    //    image   : req.body.image
+    //});
+    //submission.save(function (e) {
+    //    if (e) throw ("failed to create new submission", e);
+    //    res.end();
+    //});
 }
