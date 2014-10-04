2703995bacbc429154209626d30a44a3bca79c2a
Sammons <bsammnz@gmail.com>
2014-10-04 17:47:33 -0500

    formidable

--- OutDare/endpoints/submissions.js
+++ OutDare/endpoints/submissions.js
@@ -1,14 +1,21 @@
 ﻿var database = require('../database.js');
+var formidable = require('formidable');
 
 module.exports.url = '/submissions/create';
 module.exports.verb = 'post';
 module.exports.handler = function (req, res) {
-    console.log(Object.keys(req.body));
-    var submission = new database.submission({
-        user_id : req.body.user_id,
-        dare_id : req.body.dare_id,
-        image   : req.body.image
-    });
+    
+    var form = new formidable.IncomingForm();
+    
+    form.parse(req, function (err, fields, files) {
+        console.log(files);
+        var submission = new database.submission({
+            user_id : fields.user_id,
+            dare_id : fields.dare_id,
+            image   : files.image
+        });
+    })
+
     submission.save(function (e) {
         if (e) throw ("failed to create new submission", e);
         res.end();
